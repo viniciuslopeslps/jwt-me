f6be7d233a80cefdee147e01889f2cf0c7e9ea2a
Vinicius Lopes <viniciuslopeslps@gmail.com>
2017-11-17 11:47:04 -0200

    colocando o usuário logado.

--- src/main/java/com/jwtme/controllers/UserController.java
+++ src/main/java/com/jwtme/controllers/UserController.java
@@ -1,16 +1,22 @@
 package com.jwtme.controllers;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
 public class UserController {
 
-	@RequestMapping("/users")
-	@ResponseBody
-	public String getUsers() {
-		return "{\"users\":[{\"name\":\"Lucas\", \"country\":\"Brazil\"}," +
-		           "{\"name\":\"Jackie\",\"country\":\"China\"}]}";
-	}
+    @RequestMapping("/users")
+    @ResponseBody
+    public String getUsers(HttpServletRequest request, HttpServletResponse response) {
+        Authentication currentUser = SecurityContextHolder.getContext().getAuthentication();
+        return "{\"users\":[{\"name\":\"Lucas\", \"country\":\"Brazil\"}," +
+                "{\"name\":\"Jackie\",\"country\":\"China\"}]}";
+    }
 }
